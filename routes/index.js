d9fa8c72a7d094e33121cd980ac4bcad294368fc
mzarcogillett <mzarcogillett@gmail.com>
2018-02-12 12:16:02 +0100

    changed to single page app

--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,10 @@ var auction = require('../database/auction');
 module.exports = exports = function(app, db) {
 
 	/** VIEWS */
-	app.get('/', views.home);
-	app.get('/auction', views.auction)
-	app.get('/auction/:auctionID', views.auctionID);
-	app.get('/about', views.about);
+	app.get('/', views.auction);
+	//app.get('/auction', views.auction)
+	//app.get('/auction/:auctionID', views.auctionID);
+	//app.get('/about', views.about);
 
 	// Load auction items with MongoDB
 	app.get('/db/queryItems', auction.getItems);
